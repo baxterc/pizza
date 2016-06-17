4e12210532f011bde7bf225ec341a114c565bf48
Charlie Baxter <charlie.r.baxter@gmail.com>
2016-06-17 07:54:47 -0700

    add a topping input field that adds that topping to the pizza

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 - Create a pizza object with a size and topping using a pizza constructor.
 Sample Input: Clicks a button that constructs a pizza
-Sample Output: {"pizzaSize: medium", "pizzaTopping: extra cheese"}
+Sample Output: {pizzaSize: "medium", pizzaTopping: "extra cheese"}
+
+- Allow the user to select a single topping via a prototype.
+Sample Input: "olives"
+Sample Output: {pizzaSize: "medium", pizzaTopping: "olives"}
--- js/scripts.js
+++ js/scripts.js
@@ -5,10 +5,16 @@ function Pizza(size, topping) {
   this.pizzaTopping = topping;
 }
 
+Pizza.prototype.toppingAdd = function (topping) {
+  this.pizzaTopping = topping;
+};
+
 //UI Logic
 $("document").ready(function() {
   $("button#pizzaOven").click(function() {
     var thisPizza = new Pizza("medium", "extra cheese");
+    var topping1 = $("#topping").val();
+    thisPizza.toppingAdd(topping1);
     console.log(thisPizza);
     $(".pizzaDisplay").text(thisPizza);
   });
