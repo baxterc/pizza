df9b8ed0b1f360d9b350f054538e7af923bcb72c
Charlie Baxter <charlie.r.baxter@gmail.com>
2016-06-17 13:36:07 -0700

    fix a small typo that kept the pizza cost from being calculated

--- js/scripts.js
+++ js/scripts.js
@@ -82,6 +82,7 @@ Pizza.prototype.toppingAdd = function (topping) {
 };
 
 Pizza.prototype.pizzaPriceCalc = function() {
+  debugger;
   var totalPrice = this.pizzaPrice;
   var baseArr = this.pizzaBases;
   var topArr = this.pizzaToppings;
@@ -94,6 +95,7 @@ Pizza.prototype.pizzaPriceCalc = function() {
     var tPrice = top.toppingPrice;
     totalPrice += tPrice;
   });
+  this.pizzPrice = totalPrice;
 };
 
 
@@ -125,19 +127,11 @@ $("document").ready(function() {
     var thisSize = new Size(newSize);
     var thisCrust = new Crust(newCrust);
     var thisSauce = new Sauce(newSauce);
-    // var t1 = $("#topping1").val();
-    // var topping1 = new Topping(t1, 1)
-    // var t2 = $("#topping2").val();
-    // var topping2 = new Topping(t2, 3)
-    // var t3 = $("#topping3").val();
-    // var topping3 = new Topping(t3, 2)
+
     thisPizza.baseAdd(thisSize);
     thisPizza.baseAdd(thisCrust);
     thisPizza.baseAdd(thisSauce);
-    // thisPizza.toppingAdd(topping1);
-    // thisPizza.toppingAdd(topping2);
-    // thisPizza.toppingAdd(topping3);
-    // pizzaPriceCalc(thisPizza);
+
     thisPizza.pizzaPriceCalc();
     // console.log(thisPizza);
     // console.log(thisPizza.pizzaTopping[0]);
